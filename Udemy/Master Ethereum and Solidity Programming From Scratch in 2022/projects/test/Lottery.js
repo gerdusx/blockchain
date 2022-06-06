a3ad4e72784c7cbf7070cb96addf525a819b8ed2
Gerdus Marais <gerdusx@gmail.com>
2022-06-06 21:57:17 +0200

    55 - Validation, The require stement

--- Udemy/Master Ethereum and Solidity Programming From Scratch in 2022/projects/test/Lottery.js
+++ Udemy/Master Ethereum and Solidity Programming From Scratch in 2022/projects/test/Lottery.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { ethers } = require("hardhat");
+const { ethers, waffle } = require("hardhat");
 
 describe("Lottery", () => {
     let Lottery;
@@ -12,12 +12,42 @@ describe("Lottery", () => {
         Lottery = await ethers.getContractFactory("Lottery");
         [owner, addr1, addr2] = await ethers.getSigners();
         lotteryContract = await Lottery.deploy();
+        await lotteryContract.deployed();
     })
 
-    describe("Deployement", async () => {
+    describe("Deployment", async () => {
         it("Should assign the correct manager", async () => {
-            console.log('dsfsdf');
             expect(await lotteryContract.manager()).to.equal(owner.address);
+        });
+
+        it("Contract balance should be zero", async () => {
+            expect(await lotteryContract.getBalance()).to.equal(0);
+        });
+
+        it("Only the manager should be able to view balance", async () => {
+            await expect(lotteryContract.connect(addr1).getBalance()).to.be.revertedWith("Unauthorized");
+        });
+    });
+
+    describe("Entering the lottery", async () => {
+        
+        it("Contract amount should be updated after a deposit", async () => {
+            await addr1.sendTransaction({
+                    to: lotteryContract.address,
+                    value: ethers.utils.parseEther("0.1")
+                })
+    
+            expect(await lotteryContract.getBalance()).to.equal(ethers.utils.parseEther("0.1"));
+        });
+
+        it("Contestant should be added to the list of players", async () => {
+            await addr1.sendTransaction({
+                    to: lotteryContract.address,
+                    value: ethers.utils.parseEther("0.1")
+                })
+    
+            expect(await lotteryContract.players([0])).to.equal(addr1.address);
         })
+
     })
 })
