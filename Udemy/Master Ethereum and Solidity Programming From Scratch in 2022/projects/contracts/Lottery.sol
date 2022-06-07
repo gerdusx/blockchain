cf89bd5dc0d5e26c5be4fe16a0ba0ea845bcf2d1
Gerdus Marais <gerdusx@gmail.com>
2022-06-07 14:30:46 +0200

    59 - Resetting the lottery

--- Udemy/Master Ethereum and Solidity Programming From Scratch in 2022/projects/contracts/Lottery.sol
+++ Udemy/Master Ethereum and Solidity Programming From Scratch in 2022/projects/contracts/Lottery.sol
@@ -10,7 +10,7 @@ contract Lottery {
     }
 
     receive() external payable {
-        require(msg.value == 0.1 ether);
+        require(msg.value == 0.1 ether, "Please deposit 0.1 ether");
         players.push(payable(msg.sender));
     }
 
@@ -18,4 +18,22 @@ contract Lottery {
         require(msg.sender == manager, "Unauthorized");
         return address(this).balance;
     }
+
+    function random() public view returns(uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+    }
+
+    function pickWinner() public {
+        require(msg.sender == manager, "Unauthorized");
+        require(players.length >= 3, "Mininum of 3 players required");
+
+        uint randomNumber = random();
+        address payable winner;
+
+        uint index = randomNumber % players.length;
+        winner = players[index];
+        winner.transfer(getBalance());
+        players = new address payable[](0); //resetting the lottering
+    }
+
 }
--- Udemy/Master Ethereum and Solidity Programming From Scratch in 2022/projects/test/Lottery.js
+++ Udemy/Master Ethereum and Solidity Programming From Scratch in 2022/projects/test/Lottery.js
@@ -7,10 +7,11 @@ describe("Lottery", () => {
     let owner;
     let addr1;
     let addr2;
+    let addr3;
 
     beforeEach(async () => {
         Lottery = await ethers.getContractFactory("Lottery");
-        [owner, addr1, addr2] = await ethers.getSigners();
+        [owner, addr1, addr2, addr3] = await ethers.getSigners();
         lotteryContract = await Lottery.deploy();
         await lotteryContract.deployed();
     })
@@ -40,6 +41,13 @@ describe("Lottery", () => {
             expect(await lotteryContract.getBalance()).to.equal(ethers.utils.parseEther("0.1"));
         });
 
+        it("Should only accept 0.1 ether as entry", async () => {
+            await expect(addr1.sendTransaction({
+                to: lotteryContract.address,
+                value: ethers.utils.parseEther("0.2")
+            })).to.be.revertedWith("Please deposit 0.1 ether");
+        });
+
         it("Contestant should be added to the list of players", async () => {
             await addr1.sendTransaction({
                     to: lotteryContract.address,
@@ -49,5 +57,33 @@ describe("Lottery", () => {
             expect(await lotteryContract.players([0])).to.equal(addr1.address);
         })
 
+    });
+
+    describe("Picking a winner", async () => {
+        it ("Only manager should be able to pick a winner", async () => {
+            await (expect(lotteryContract.connect(addr1).pickWinner()).to.be.revertedWith("Unauthorized"));
+        });
+
+        it ("Minimum of 3 players required", async () => {
+            await (expect(lotteryContract.pickWinner()).to.be.revertedWith("Mininum of 3 players required"));
+        });
+
+        it ("Contract balance should be zero after winner is selected", async () => {
+            await addr1.sendTransaction({ to: lotteryContract.address, value: ethers.utils.parseEther("0.1") })
+            await addr2.sendTransaction({ to: lotteryContract.address, value: ethers.utils.parseEther("0.1") })
+            await addr3.sendTransaction({ to: lotteryContract.address, value: ethers.utils.parseEther("0.1") })
+
+            await lotteryContract.pickWinner();
+            expect(await lotteryContract.getBalance()).to.equal(0);
+        });
+
+        it ("Users should be resetted after a winner is selected", async () => {
+            await addr1.sendTransaction({ to: lotteryContract.address, value: ethers.utils.parseEther("0.1") })
+            await addr2.sendTransaction({ to: lotteryContract.address, value: ethers.utils.parseEther("0.1") })
+            await addr3.sendTransaction({ to: lotteryContract.address, value: ethers.utils.parseEther("0.1") })
+
+            await lotteryContract.pickWinner();
+            await (expect(lotteryContract.pickWinner()).to.be.revertedWith("Mininum of 3 players required"));
+        });
     })
 })
