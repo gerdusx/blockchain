694040497452c53b7932b933063cab0d06020d5b
Gerdus Marais <gerdusx@gmail.com>
2022-07-21 16:27:16 +0200

    fund me html final

--- Courses/Hardhat - FreeCodeCamp/html-fund-me/index.js
+++ Courses/Hardhat - FreeCodeCamp/html-fund-me/index.js
@@ -3,9 +3,14 @@ import { abi, contractAddress } from "./constants.js";
 
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton");
+const balanceButton = document.getElementById("balanceButton");
+const withdrawButton = document.getElementById("withdrawButton");
+const fundInput = document.getElementById("ethAmount");
 
 connectButton.onclick = connect;
 fundButton.onclick = fund;
+balanceButton.onclick = getBalance;
+withdrawButton.onclick = withdraw;
 
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
@@ -25,8 +30,33 @@ async function connect() {
     }
 }
 
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const balance = await provider.getBalance(contractAddress);
+        console.log(ethers.utils.formatEther(balance));
+    }
+}
+
+async function withdraw() {
+    if (typeof window.ethereum !== "undefined") {
+        console.log(`Withdrawing...`);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+
+        try {
+            const transactionResponse = await contract.withdraw();
+            await listenForTransactionMine(transactionResponse, provider);
+            console.log("Withdraw Done!!");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
 async function fund() {
-    const ethAmount = "77";
+    const ethAmount = fundInput.value;
     if (typeof window.ethereum !== "undefined") {
         console.log(`Funding with ${ethAmount}`);
         const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -39,7 +69,7 @@ async function fund() {
         try {
             const transactionResponse = await contract.fund({ value: ethers.utils.parseEther(ethAmount) });
             await listenForTransactionMine(transactionResponse, provider);
-            console.log("Done!!");
+            console.log("Funding Done!!");
         } catch (error) {
             console.log(error);
         }
@@ -54,5 +84,5 @@ function listenForTransactionMine(transactionResponse, provider) {
             console.log(`Completed with ${transactionReceipt.confirmations} confirmations`);
             resolve();
         });
-    })
+    });
 }
